b0450c1d1f63d00ed7f51cf7ff94ce65c9e10d19
Federico Simonetta <federico.simonetta@unimi.it>
2021-04-05 14:10:51 +0200

    Added html files

--- list_cy_annotations.py
+++ list_cy_annotations.py
@@ -1,22 +1,19 @@
 # distutils: language=c++
 
-from typing import List
 import cython
 
 
-def iterate_list(a_list: List[List[float]]):
+@cython.locals(i=cython.int, j=cython.int, count=cython.float, a_list=list)
+def iterate_list(a_list: list):
 
-    count = cython.declare(cython.float, 0.0)
-    i = cython.declare(cython.int, 0)
-    j = cython.declare(cython.int, 0)
+    count = 0.0
     for i in range(len(a_list)):
         for j in range(len(a_list[i])):
-            count *= a_list[i][j]
+            count = count * a_list[i][j]
 
 
-def make_list(a_list: List[List[float]]):
-    i = cython.declare(cython.int, 0)
-    j = cython.declare(cython.int, 0)
+@cython.locals(i=cython.int, j=cython.int, new_list=list, a_list=list)
+def make_list(a_list: list):
     for i in range(10**4):
         new_list = []
         for j in range(10**4):
